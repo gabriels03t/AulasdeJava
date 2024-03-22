62c4bc4a4b7e55bfeb2237253f70b1e5975b1ab3
gabriels03t <gabrielsouzaturecki@gmail.com>
2024-03-22 20:54:49 -0300

    última alteração da conta bancaria

--- Ex3 - Conta Bancaria/ContaBancaria.java
+++ Ex3 - Conta Bancaria/ContaBancaria.java
@@ -1,6 +1,33 @@
-package ContaBancaria.java;
-
 public class ContaBancaria {
-    
-    
+    private int numeroConta;
+    private String nomeTitular;
+    private double saldo;
+
+    public ContaBancaria(int numeroConta, String nomeTitular, double saldo) {
+        this.numeroConta = numeroConta;
+        this.nomeTitular = nomeTitular;
+        this.saldo = saldo;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void sacar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente para realizar o saque.");
+        }
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void exibirInfo() {
+        System.out.println("Número da Conta: " + numeroConta);
+        System.out.println("Nome do Titular: " + nomeTitular);
+        System.out.println("Saldo: " + saldo);
+    }
 }
--- Ex3 - Conta Bancaria/Main.java
+++ Ex3 - Conta Bancaria/Main.java
@@ -1,5 +1,10 @@
-package ContaBancaria.java;
-
 public class Main {
-    
+    public static void main(String[] args) {
+        ContaBancaria conta1 = new ContaBancaria(123456, "João", 1000.0);
+        ContaBancaria conta2 = new ContaBancaria(789012, "Maria", 2000.0);
+
+        conta1.exibirInfo();
+        System.out.println();
+        conta2.exibirInfo();
+    }
 }
